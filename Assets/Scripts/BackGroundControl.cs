737f75dc3cfff6cdcc5ebeaf2959f52861f1e1d0
YusukeY <87760101+YYramen@users.noreply.github.com>
2021-09-16 00:58:42 +0900

    色々追加した

---
+++ Assets/Scripts/BackGroundControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float speed = 1;
+    SpriteRenderer m_sprite = default;
+
+    private void Start()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //横方向にスクロール
+        transform.position -= new Vector3(Time.deltaTime, 0 * speed);
+        float width = m_sprite.bounds.size.x;
+        if (transform.position.x < -width)
+        {
+            transform.position += Vector3.back * width * 2;
+        }
+    }
+}
