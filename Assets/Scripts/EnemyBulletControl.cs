6c2e0868777d60482d75a0bc803bb8a188f55d5f
YusukeY <87760101+YYramen@users.noreply.github.com>
2021-09-17 17:03:58 +0900

    一旦

--- Assets/Scripts/EnemyBulletControl.cs
+++ Assets/Scripts/EnemyBulletControl.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBulletController : MonoBehaviour
+public class EnemyBulletControl : MonoBehaviour
 {
+    [SerializeField] float m_bulletSpeed = 5f;
+    GameObject m_player = default;
+    [SerializeField] GameObject m_effectPrefab = default;
+
+    float m_angleOffset = 0f;
+    Rigidbody2D m_rb = default;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody2D>();
+        m_player = GameObject.Find("Player");
+        //m_effectPrefab = GameObject.Find("OctopusBulletEffect");
+
+        if (m_player)
+        {
+            Vector3 dir = m_player.transform.position - transform.position;
+            //角度を取得
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Vector3 euler = new Vector3(0, 0, angle + m_angleOffset);
+            transform.rotation = Quaternion.Euler(euler);
+            m_rb.velocity = dir.normalized * m_bulletSpeed;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (m_effectPrefab && collision.CompareTag("Player"))
+        {
+            Instantiate(m_effectPrefab, this.transform.position, this.transform.rotation);
+        }
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
 {
+    [SerializeField] GameObject m_enemyBulletPrefab = default;
+    [SerializeField] Transform m_muzzle = null;
+
+    float m_targetTime = 1.0f;
+    float m_currentTime = 0;
+    Animator m_anim = default;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_currentTime += Time.deltaTime;
+        if (m_targetTime < m_currentTime)
+        {
+            Instantiate(m_enemyBulletPrefab, m_muzzle.position, m_enemyBulletPrefab.transform.rotation);
+            m_currentTime = 0;
+        }
     }
 }
